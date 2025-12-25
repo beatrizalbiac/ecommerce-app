f22f79cbe3c77a1965dfa38c8ae6b70ff7213d19
beatrizalbiac <alu.161043@usj.es>
2025-12-25 20:02:47 +0100

    added created at (forgot to add it b4)

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -1,20 +1,23 @@
 from sqlmodel import SQLModel, Field
 from pydantic import EmailStr
+from datetime import datetime, timezone, timedelta
 
 
 class UserBase(SQLModel):
     name: str = Field(index=True)
     lastname: str = Field(index=True)
-    email: EmailStr = Field(index=True)
+    email: EmailStr = Field(index=True) 
 
 class User(UserBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     password: str
     is_admin: bool = Field(default=False)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc) + timedelta(hours=1)) # to make it UTC+1 (The spanish time)
 
 class UserPublic(UserBase):
     id: int
     is_admin: bool
+    created_at: datetime
 
 class UserCreate(UserBase):
     password: str
